55736544ec0f67c3b51965ce2c9e2bd3702de4f9
Ryan Thomas <ryan@fl0.com>
2023-01-31 10:19:45 +1100

    Initial

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::{net::SocketAddr, sync::Arc};
+
+use axum::{extract::State, Json, Router};
+use log::info;
+use serde_json::Value;
+use tokio::sync::Mutex;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    let port = 3000;
+    info!("starting test service on port: {port}");
+
+    let state = CountState {
+        counter: Arc::new(Mutex::new(0)),
+    };
+
+    let app = Router::new()
+        .route("/", axum::routing::get(root))
+        .with_state(state);
+
+    axum::Server::bind(&SocketAddr::from(([0, 0, 0, 0], port)))
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
+}
+
+async fn root(state: State<CountState>) -> Json<Value> {
+    let mut c = state.0.counter.lock().await;
+    *c += 1;
+
+    Json(serde_json::json!({ "counter": *c }))
+}
+
+#[derive(Clone)]
+struct CountState {
+    counter: Arc<Mutex<usize>>,
+}
